79dede788ee196c97cf8850841ec0a115adc03e4
Joshua Baker <joshuabaker2@gmail.com>
2016-04-07 22:21:23 -0700

    velocity and rotation

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -8,13 +8,13 @@ class Player extends Phaser.Sprite {
         super(game, x, y, 'kraken');
         this.scale.setTo(0.01, 0.01);
         this.cursors = game.input.keyboard.createCursorKeys();
-
+        this.anchor.setTo(0.5, 0.5);
 
         //  We need to enable physics on the player
         game.physics.arcade.enable(this);
 
         //  Player physics properties.
-        // this.body.collideWorldBounds = true;
+        this.body.collideWorldBounds = true;
 
         //  TODO: Our two animations, walking left and right.
 //    player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -23,8 +23,9 @@ class Player extends Phaser.Sprite {
 
     update () {
         // gets called automatically by World.update()
-        this.handleMomentum();
         this.handleKeyboard();
+        this.handleMomentum();
+
     }
 
     handleMomentum () {
@@ -39,22 +40,32 @@ class Player extends Phaser.Sprite {
         } else if (this.body.velocity.y < 0) {
             this.body.velocity.y += 2;
         }
+
+        this.body.acceleration = 0;
+
+
+
     }
 
     handleKeyboard () {
 
+
+
         if (this.cursors.left.isDown) {
-            this.body.velocity.x -= 4;
-            // this.animations.play('left');
+            this.body.angularVelocity = this.body.angularVelocity > -200 ? this.body.angularVelocity - 4 : this.body.angularVelocity;
         } else if (this.cursors.right.isDown) {
-            this.body.velocity.x += 4;
-            // this.animations.play('right');
+            this.body.angularVelocity = this.body.angularVelocity < 200 ? this.body.angularVelocity + 4 : this.body.angularVelocity;
+        } else {
+            this.body.angularVelocity = 0;
         }
 
+
         if (this.cursors.up.isDown) {
-            this.body.velocity.y -= 4;
+            const tempVelocity = this.game.physics.arcade.velocityFromAngle(this.angle + 90, 4);
+            this.body.velocity.x += tempVelocity.x;
+            this.body.velocity.y += tempVelocity.y;
         } else if (this.cursors.down.isDown) {
-            this.body.velocity.y += 4;
+            this.game.physics.arcade.velocityFromAngle(this.angle + 90, -15, this.body.velocity);
         }
     }
 
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -11,15 +11,10 @@ class GameState extends Phaser.State {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY };
 		let player = new Player(this.game, center.x, center.y);
 		this.game.add.existing(player);
-		// let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		// text.anchor.set(0.5);
 	}
 
 	update() {
-		// player.handleKeyboard();
-//    _handleCollisions();
-// 	player.handleMomentum();
-//    handleKeyboard();
+
 	}
 
 }
